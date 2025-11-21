5e699d08793f1fd6bc0e2e6b9f9c263df5c526dc
Đặng Trọng Phúc <104264406+dangphuc1902@users.noreply.github.com>
2025-11-21 07:37:39 +0700

    Fix: Move proto from shared-utils to libs/fpm-protocol

---
+++ fpm-libs/fpm-protocol/src/test/java/com/fpm2025/fpm_protocol/FpmProtocolApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fpm2025.fpm_protocol;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FpmProtocolApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
